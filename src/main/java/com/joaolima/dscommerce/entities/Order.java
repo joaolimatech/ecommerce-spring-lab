592e92f70b7f272c9130aa4b7b0c55b41b311b7a
Joao Lima <joaolimatech@gmail.com>
2025-08-10 15:09:52 -0300

    Subindo projeto

---
+++ src/main/java/com/joaolima/dscommerce/entities/Order.java
@@ -0,0 +1,53 @@
+package com.joaolima.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name="tb_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+    private OrderStatus status;
+
+    //Quando formos fazer um relacionamento Muitos:1, devemos usar a anotacao:
+    @ManyToOne //pois um pedido só pode pertencer a 1 cliente
+    @JoinColumn(name="client _id") //FK adiconada na table tb_name
+    private User client;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+}
