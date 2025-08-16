54f12d4fcbf367d3296df599d0781eaf65ae1f7c
Joao Lima <joaolimatech@gmail.com>
2025-08-16 20:45:19 -0300

    Equals and hashCode

--- src/main/java/com/joaolima/dscommerce/entities/OrderItemPK.java
+++ src/main/java/com/joaolima/dscommerce/entities/OrderItemPK.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+import java.util.Objects;
+
 //Criamos essa classe pq esse objeto será uma Chave Primaria composta, recebendo Pk de Order e de Product
 @Embeddable  //Fala q será uma entidade nao a virar uma tabela, mas sim para ser adc em uma talvez
 public class OrderItemPK {
@@ -32,5 +34,19 @@ public class OrderItemPK {
 
     public void setProduct(Product product) {
         this.product = product;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof OrderItemPK that)) return false;
+
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(order);
+        result = 31 * result + Objects.hashCode(product);
+        return result;
     }
 }
